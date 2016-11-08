c108a4e1235a910cb52313d2171edee142f7da39
Chris DeMoll <demoll.chris@gmail.com>
2016-11-08 16:00:58 -0500

    weight letter selection

--- browser/react/action-creators/GameActions.js
+++ browser/react/action-creators/GameActions.js
@@ -17,10 +17,31 @@ export const saveRound = function () {
   return action;
 };
 
+const pickRandomLettersByWeight = numLetters => {
+  const dictionary = {
+    A: 9, B: 3, C: 3, D: 4, E: 12, F: 2, G: 3, H: 2, I: 9, J: 1, K: 2, L: 4, M: 3,
+    N: 6, O: 8, P: 3, Q: 1, R: 6, S: 4, T: 6, U: 4, V: 2, W: 2, X: 1, Y: 2, Z: 1
+  }
+
+  let freqArr = [];
+
+  for (let letter in dictionary) {
+    freqArr = freqArr.concat(new Array(dictionary[letter]).fill(letter));
+  }
+
+  let output = '';
+
+  for (let i = 0; i < numLetters; i++) {
+    output += freqArr[Math.floor(Math.random() * freqArr.length)];
+  }
+
+  return output;
+}
+
 export const generateRound = function () {
   const thunk = function (dispatch) {
     let numLetters = Math.ceil(Math.random() * 5) + 2;
-    let acronym = Math.random().toString(36).replace(/[^a-z]+/g, '').substring(0, numLetters).toUpperCase();
+    let acronym = pickRandomLettersByWeight(numLetters);
     let round = {
       acronym,
       winner: null,
