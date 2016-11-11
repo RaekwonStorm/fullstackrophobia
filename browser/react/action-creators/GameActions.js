830ba49f3d048cae59771beffe71383a83cdec13
Reed Branson <reed.branson@gmail.com>
2016-11-11 16:47:50 -0500

    completed user model and test spec

--- browser/react/action-creators/GameActions.js
+++ browser/react/action-creators/GameActions.js
@@ -17,18 +17,19 @@ export const saveRound = function () {
   return action;
 };
 
+const dictionary = {
+  A: 12, B: 5, C: 4, D: 3, E: 2, F: 4, G: 2, H: 7, I: 6, J: 1, K: 1, L: 3, M: 4,
+  N: 2, O: 6, P: 3, Q: 1, R: 2, S: 8, T: 17, U: 2, V: 1, W: 7, X: 1, Y: 2, Z: 1
+}
+
+let freqArr = [];
+
+for (let letter in dictionary) {
+  freqArr = freqArr.concat(new Array(dictionary[letter]).fill(letter));
+}
+
+
 const pickRandomLettersByWeight = numLetters => {
-  const dictionary = {
-    A: 9, B: 3, C: 3, D: 4, E: 12, F: 2, G: 3, H: 2, I: 9, J: 1, K: 2, L: 4, M: 3,
-    N: 6, O: 8, P: 3, Q: 1, R: 6, S: 4, T: 6, U: 4, V: 2, W: 2, X: 1, Y: 2, Z: 1
-  }
-
-  let freqArr = [];
-
-  for (let letter in dictionary) {
-    freqArr = freqArr.concat(new Array(dictionary[letter]).fill(letter));
-  }
-
   let output = '';
 
   for (let i = 0; i < numLetters; i++) {
